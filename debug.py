5b668f9cf9847699912efec39bd5b5f10fefac97
Chris Tralie <chris.tralie@gmail.com>
2023-09-20 20:13:59 -0400

    Starter code

---
+++ debug.py
@@ -0,0 +1,158 @@
+"""
+Programmer: Chris Tralie
+Purpose: Some utilities to help with debugging, including conversion to
+JFLAP and a method to test all strings up to a certain size in an alphabet
+against Python's built-in regular expression evaluator
+"""
+
+import numpy as np
+
+def get_state_info(delta, width):
+    """
+    Determine the states and their ids/positions
+
+    Parameters
+    ----------
+    delta: Dictionary: (String, String) -> String
+        Transition function from (state, character) -> state
+        Assumed to follow the naming conventions in HW4
+    width: float
+        Width in which to put states
+    
+    Returns
+    -------
+    pos: string->(float, float)
+        Positions of each state
+    ids: string->int
+        IDs of each state
+    """
+    states = set([key[0] for key in delta.keys()])
+    for v in delta.values():
+        states.update(v)
+    counts = [sum([c == "_" for c in key]) for key in states]
+    ## Step 1: Figure out order of singleton keys
+    keys_single = [key for (key, count) in zip(states, counts) if count == 1]
+    keys_single = sorted(keys_single, key=lambda x: int(x[2::]))
+    following = {k:[] for k in keys_single}
+
+    for k in keys_single:
+        for k2 in states:
+            if len(k2) > len(k) and k2[0:len(k)] == k:
+                following[k].append(k2)
+    
+    ids = {}
+    idx = 1
+    for k in keys_single:
+        ids[k] = idx
+        idx += 1
+        for k2 in sorted(following[k], key=lambda x: -len(x)):
+            ids[k2] = idx
+            idx += 1
+    ids["Start"] = 0
+    ids["Final"] = len(ids)+1
+    pos = {}
+    for k, id in ids.items():
+        theta = np.pi*id/len(ids)
+        pos[k] = (width*(1-np.cos(theta)), width*np.sin(theta))
+    return pos, ids
+    
+
+
+def write_dfa_jflap(delta, filename):
+    """
+    Programmer: Chris Tralie
+    Convert a python dictionary describing DFA transitions into
+    XML that JFLAP understands
+
+    Parameters
+    ----------
+    delta: Dictionary: (String, String) -> String
+        Transition function from (state, character) -> state
+        Assumed to follow the naming conventions in HW4
+    filename: string
+        Path to which to write JFLAP file
+    """
+    import numpy as np
+    BEGIN_JFF = """<?xml version="1.0" encoding="UTF-8" standalone="no"?><!--Created with substring.py in Ursinus College CS 373--><structure>
+        <type>fa</type>
+        <automaton>"""
+    ## Step 1: Setup the states
+    jff_string = "" + BEGIN_JFF
+    states_xml = ""
+
+    delta_orig = delta
+    # Convert to (State):(Character:State)
+    delta = {}
+    for (state, c), state_to in delta_orig.items():
+        if not state in delta:
+            delta[state] = {}
+        delta[state][c] = state_to
+    pos, ids = get_state_info(delta_orig, width=400)
+    states = list(pos.keys())
+
+    ## Step 1b: Setup state positions
+    for state in states:
+        id = ids[state]
+        (x, y) = pos[state]
+        d = ""
+        if state == "Start":
+            d = "<initial/>"
+        if state == "Final":
+            d = "<final/>"
+        states_xml += "<state id=\"{}\" name=\"{}\">\n    <x>{}</x>    <y>{}</y>\n{}</state>".format(id, state, x, y, d)
+    ## Step 2: Setup the transitions
+    jff_string += states_xml
+    for state_from in delta.keys():
+        state_from_id = ids[state_from]
+        for c in delta[state_from].keys():
+            for state_to in delta[state_from][c]:
+                state_to = ids[state_to]
+                jff_string += "<transition>\n    <from>{}</from>\n    <to>{}</to>\n    ".format(state_from_id, state_to)
+                if c:
+                    jff_string += "<read>{}</read>".format(c)
+                else:
+                    # Lambda arrow
+                    jff_string += "<read/>"
+                jff_string += "</transition>\n"
+    jff_string += "</automaton></structure>"
+    fout = open(filename, "w")
+    fout.write(jff_string)
+    fout.close()
+
+
+def cmp_python(rexp, Sigma, max_len):
+    """
+    Compare the results of evaluating a regular expression
+    on all strings using an alphabet up to a particular length
+
+    Parameters
+    ----------
+    rexp: string
+        The regular expression
+    Sigma: list of char
+        The alphabet
+    max_len: int
+        Maximum length of string
+    """
+    from re import fullmatch # Python's version
+    from mygrep import eval_regexp
+    queue = [""]
+    finished = False
+    # Go through strings in lexicographic order, as per the order in Sigma
+    correct = 0
+    tried = 0
+    while not finished:
+        s = queue.pop(0)
+        if len(s) > max_len:
+            finished = True
+        else:
+            tried += 1
+            myres = eval_regexp(rexp, s)
+            pyres = fullmatch(rexp, s) is not None
+            if myres != pyres:
+                print("Wrong on {}: Mine {}, Python {}".format(s, myres, pyres))
+            else:
+                correct += 1
+            for c in Sigma:
+                queue.append(s+c)
+    print("{} / {} Correct on {} up to length {}".format(correct, tried, rexp, max_len))
---
+++ mygrep.py
@@ -0,0 +1,202 @@
+def eval_nfa(s, q, delta, F, verbose=False):
+    """
+    Evaluate a standard NFA with no lambda arrows
+     
+    Parameters
+    ----------
+    s: string
+        Input to try
+    q: string
+        Start state
+    delta: (string, string) -> set([string])
+        Transition function
+    F: set([string])
+        Accept states
+    verbose: bool
+        If True, print info about sequence of states that are visited
+    """
+    states = set([q]) ## We now track multiple possibilities
+    if verbose:
+        print("Start", states)
+    for c in s:
+        next_states = set([])
+        for p in states:
+            if (p, c) in delta:
+                ## This line is different from DFA; there are multiple
+                ## states we can branch out to; track the unique ones
+                next_states = next_states.union(delta[(p, c)])
+        states = next_states
+        if verbose:
+            print(c, states)
+    ## At least one state must be in the set of accept states F
+    return len(states.intersection(F)) > 0
+
+
+def get_reachable_lambdas(start, delta):
+    """
+    Parameters
+    ----------
+    start: string
+        State at which to start.  We'll gather anything
+        that can be reached by a sequence of lambdas
+        from this state
+    delta: (string, string) -> set([string])
+        Transition function
+     
+    Returns
+    -------
+    set([string])
+        Set of all states reachable from start 
+        by a sequence of lambda arrows
+    """
+    reachable = set([])
+    stack = [start]
+    while len(stack) > 0:
+        state = stack.pop()
+        reachable.add(state)
+        if (state, None) in delta:
+            # If there are lambda arrows out of this state, 
+            # loop through all of them
+            for neighbor in delta[(state, None)]:
+                if not neighbor in reachable:
+                    # If we haven't seen this state yet 
+                    # (Crucial to avoid infinite loops for lambda cycles)
+                    stack.append(neighbor)
+    reachable.remove(start) # Exclude the start state itself
+    return reachable
+
+
+def reduce_nfa_lambdas(delta, F):
+    """
+    Eliminate the lambdas in an NFA by doing a reduction
+    to equivalent arrows and accept states to without 
+    lambdas
+     
+    Parameters
+    ----------
+    delta: (string, string) -> set([string])
+        Transition function, which will be updated as as side effect
+    F: set([string])
+        Final states, which will be updated as as side effect
+    """
+    ## Step 1: Copy the dictionary into a new format where
+    ## it's more convenient to look up only the arrows coming
+    ## out of a particular state
+    delta_state = {} #(State):(Character:set(states))
+    for (state, c), states_to in delta.items():
+        if not state in delta_state:
+            delta_state[state] = {}
+        if not c in delta_state[state]:
+            delta_state[state][c] = set([])
+        delta_state[state][c].update(states_to)
+
+    ## Step 2: Loop through each state, get the new arrows, and 
+    ## update the final states
+    states = set([key[0] for key in delta])
+    F_new = set([])
+    for state in states:
+        # Get states that are reachable from this state
+        reachable = get_reachable_lambdas(state, delta)
+        # Figure out if this should be an accept state based
+        # on what's reachable
+        if len(reachable.intersection(F)) > 0:
+            F_new.add(state)
+        # Add the new arrows
+        for other in reachable:
+            if other in delta_state: # If there are actually any arrows coming out of this state
+                for c, states_to in delta_state[other].items():
+                    if c is not None:
+                        # Add this new equivalent arrow
+                        if not (state, c) in delta:
+                            delta[(state, c)] = set([])
+                        delta[(state, c)] = delta[(state, c)].union(states_to)
+    F.update(F_new)
+     
+    ## Step 3: Remove the original lambda arrows
+    to_remove = [key for key in delta.keys() if key[1] is None]
+    for key in to_remove:
+        del delta[key]
+
+
+def infix2postfix(expr):
+    """
+    Convert an infix specification of a regular expression into
+    a postfix version.
+    If two adjacent characters do not include ()*|, 
+    then insert a . (concatenate) in between them
+    Also insert a . (concatenate) after every *
+
+    Parameters
+    ----------
+    expr: string
+        Regular expression in infix form
+    
+    Returns
+    -------
+    An array of the postfix expression, which includes ()*|. for operators,
+    and which includes "c_i" for the operands, where c is the character
+    and i is the index
+
+    Precedence: star > concatenation > union
+    """
+    ## Step 1: Convert infix string into infix list, escaping characters, 
+    ## converting to c_i, and inserting concatenation . where necessary
+    i = 0
+    idx = 0
+    infix = []
+    while i < len(expr):
+        if expr[i] in "*|()":
+            infix.append(expr[i])
+            if expr[i] == "*" and i < len(expr)-1:
+                infix.append(".")
+        else:
+            if expr[i] == "\\": # Escape character
+                i += 1
+            c = expr[i]
+            infix.append("{}_{}".format(c, idx))
+            idx += 1
+            if i < len(expr)-1 and expr[i+1] not in "*|)":
+                infix.append(".")
+        i += 1
+    
+    ## Step 2: Convert infix list to postfix list
+    precedence = {"*":2, ".":1, "|":0}
+    postfix = []
+    stack = []
+    for x in infix:
+        if x in precedence.keys():
+            while len(stack) > 0 and stack[-1] != "(" and precedence[x] <= precedence[stack[-1]]:
+                postfix.append(stack.pop())
+            stack.append(x)
+        elif x == "(":
+            stack.append(x)
+        elif x == ")":
+            while len(stack) > 0 and stack[-1] != "(":
+                postfix.append(stack.pop())
+            stack.pop()
+        else:
+            # Ordinary operand
+            postfix.append(x)
+    
+    ## Step 3: Pop the rest of the operators from the stack
+    while len(stack) > 0:
+        postfix.append(stack.pop())
+    return infix, postfix
+
+
+def eval_regexp(R, s):
+    """
+    Check to see if a string s is part of a regular expression R
+    by converting R into an NFA and running s through that NFA.
+    This is an efficient algorithm that takes O(len(R)len(s)) time
+    in the worst case
+
+    Parameters
+    ----------
+    R: string
+        Regular expression
+    s: string
+        String we're checking
+    """
+    pass
+    ## TODO: Fill this in
